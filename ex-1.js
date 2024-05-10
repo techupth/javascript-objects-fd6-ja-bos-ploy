0319ed8465cae5cc94f76f09af6b69af5ab1fe21
Ja <ja.techup@gmail.com>
2024-05-10 13:28:02 +0700

    fix: js ex1-3

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,14 @@
 // Exercise #1: Food Order
 
-// Start coding here
+const FoodOrder = {
+  orderNumber: "A0234",
+  address: "Bangkok",
+  paymentType: "Cash",
+  totalPrice: 4500,
+  restaurantName: "MK",
+};
+
+FoodOrder.totalPrice = 5000;
+FoodOrder.paymentType = "Credit Card";
+
+console.log(FoodOrder);
