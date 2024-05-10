851695b7b1c6575faf7b32d6631a46cd37e3605e
nattawara <nattawara.jtk@gmail.com>
2024-05-10 13:27:59 +0700

    submit object assignment

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,12 @@
 // Exercise #1: Food Order
-
+const foodOder = {
+  orderNumber: "A0234",
+  address: "Bangkok",
+  paymentType: "Cash",
+  totalPrice: 4500,
+  restaurantname: "MK",
+};
+foodOder.totalPrice = 5000;
+foodOder.paymentType = "Credit Card";
+console.log(foodOder);
 // Start coding here
